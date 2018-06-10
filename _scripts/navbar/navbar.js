b80fea4d31f863684fc0907711388c8585ad3748
Elijah Nathan <ely.nathan93@gmail.com>
2018-06-10 09:42:30 +0100

    Attempt media query fixes

--- _scripts/navbar/navbar.js
+++ _scripts/navbar/navbar.js
@@ -2,10 +2,8 @@
 var toggle = document.querySelector(".toggle");
 var topNav = document.querySelector(".nav--top");
 
-
 // Listen for click event on toggle var
 toggle.addEventListener('click', function() {
-  console.log("toggle clicked");
   // toggle class "active" on topNav var
   topNav.classList.toggle("active");
 }, false);
