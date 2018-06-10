82fe253a83be2a6cdae647054824f20b3454248b
Elijah Nathan <ely.nathan93@gmail.com>
2018-06-10 10:26:11 +0100

    Fix breakpoint mixins and create navbar

--- _scripts/navbar/navbar.js
+++ _scripts/navbar/navbar.js
@@ -1,6 +1,6 @@
 // for IE10, .getElementsByClassName()[0];
 var toggle = document.querySelector(".toggle");
-var topNav = document.querySelector(".nav--top");
+var topNav = document.querySelector(".navbar--top");
 
 // Listen for click event on toggle var
 toggle.addEventListener('click', function() {
